666913ce9440511123adffb92699530305940c16
Joey van der Valk <joeyvandervalk@gmail.com>
2022-03-25 16:30:38 +0100

    ehahdshdasdjawh

--- Program.cs
+++ Program.cs
@@ -1,12 +1,68 @@
-﻿using System;
-
-namespace C__Practice
+﻿namespace C__Practice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int page = 0;
+            string input;
+            while(true)
+            {
+                switch(page)
+                {
+                    case 0:
+                        System.Console.Clear();
+                        System.Console.WriteLine("PAGE 0");
+
+                        System.Console.WriteLine("Enter action: ");
+                        input = System.Console.ReadLine();
+
+                        if(input == "next"){
+                            page = 1;
+                            input = "";
+                        }
+                        break;
+                    
+                    case 1:
+                        System.Console.Clear();
+                        System.Console.WriteLine("Page 1");
+
+                        System.Console.WriteLine("Enter action: ");
+                        input = System.Console.ReadLine();
+
+                        if(input == "next")
+                        {
+                            page = 2;
+                            input = "";
+                        } else if (input == "back")
+                        {
+                            page = 0;
+                            input = "";
+                        }
+                            
+                        
+                        break;
+
+                    case 2:
+                        System.Console.Clear();
+                        System.Console.WriteLine("Page 2");
+
+                        System.Console.WriteLine("Enter action: ");
+                        input = System.Console.ReadLine();
+
+                        if(input == "next")
+                        {
+                            page = 2;
+                            input = "";
+                        } else if (input == "back")
+                        {
+                            page = 1;
+                            input = "";
+                        }
+                        break;
+                    
+                }
+            }
         }
     }
 }
